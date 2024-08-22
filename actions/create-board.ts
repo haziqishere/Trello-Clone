01d0a53baabe47ee52d89941d00c196a9c31dce1
Haziq Hakimi Mazlisham <95128744+haziqishere@users.noreply.github.com>
2024-08-23 01:34:06 +0800

    Form component

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -4,21 +4,47 @@ import { z } from "zod";
 
 import { db }  from "@/lib/db";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export type State = {
+  errors?: {
+    title?: string[];
+  },
+  messgae?: string | null;
+}
 
 const CreateBoard = z.object({
-    title: z.string(),
+    title: z.string().min(3, {
+      message: "Minimum length of 3 letters are required",
+    })
 });
 
-export async function create(formData: FormData) {
-    const { title} = CreateBoard.parse({
+export async function create(prevState: State, formData: FormData) {
+    const validatedField = CreateBoard.safeParse({
         title: formData.get("title"),
     });
 
-    await db.board.create({
-      data: {
-        title,
-      },
-    });
+    if (!validatedField.success) {
+      return {
 
-    revalidatePath("/organization/org_2kxOMD0ntA3cM6exOQcc1ZxfZYS")
+        errors: validatedField.error.flatten().fieldErrors,
+        message: "Missing fields"
+      }
+    }
+
+    const { title } = validatedField.data;
+
+    try {
+      await db.board.create({
+        data: {
+          title,
+        },
+      });
+    }catch (error) {
+      return {
+        message: "Database error",
+      }
+    }
+    revalidatePath("/organization/org_2kxOMD0ntA3cM6exOQcc1ZxfZYS");
+    redirect("/organization/org_2kxOMD0ntA3cM6exOQcc1ZxfZYS");
   }
