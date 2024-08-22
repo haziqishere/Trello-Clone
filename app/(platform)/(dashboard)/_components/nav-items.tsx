56b14f5e5582659ea132bb4d0d6caff2dbbce748
Haziq Hakimi Mazlisham <95128744+haziqishere@users.noreply.github.com>
2024-08-22 21:42:36 +0800

    Add skeleton for loading state

--- app/(platform)/(dashboard)/_components/nav-items.tsx
+++ app/(platform)/(dashboard)/_components/nav-items.tsx
@@ -12,6 +12,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 import { useRouter, usePathname } from "next/navigation";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type Organization = {
   id: string;
@@ -102,6 +103,16 @@ export const NavItem = ({
         ))}
       </AccordionContent>
     </AccordionItem>
+  );
+};
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute" />
+      </div>
+    </div>
   );
 };
 
