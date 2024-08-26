c0d2ce598e8e7d869a9ac3c8c6396c021f5b9ac2
Haziq Hakimi Mazlisham <95128744+haziqishere@users.noreply.github.com>
2024-08-26 16:19:31 +0800

    Add a button to add list

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,5 +1,46 @@
-const BoardIdPage = () => {
-  return <div>Board ID!</div>;
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { ListContainer } from "./_components/list-container";
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      card: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={params.boardId} data={lists}></ListContainer>
+    </div>
+  );
 };
 
 export default BoardIdPage;
