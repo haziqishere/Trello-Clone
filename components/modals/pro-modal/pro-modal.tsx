28b2b949d8b9a6e5f6b2d7986a6acee162ee40ac
Haziq Hakimi Mazlisham <95128744+haziqishere@users.noreply.github.com>
2024-09-03 16:36:47 +0800

    payment reroute

--- components/modals/pro-modal/pro-modal.tsx
+++ components/modals/pro-modal/pro-modal.tsx
@@ -4,9 +4,25 @@ import Image from "next/image";
 import { useProModal } from "@/hooks/use-pro-modal";
 import { Dialog, DialogContent } from "../../ui/dialog";
 import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/use-action";
+import { stripeRedirect } from "@/actions/sripe-redirect";
+import { toast } from "sonner";
 
 export const ProModal = () => {
   const proModal = useProModal();
+
+  const { execute, isLoading } = useAction(stripeRedirect, {
+    onSuccess: (data) => {
+      window.location.href = data;
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onClick = () => {
+    execute({});
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -34,7 +50,12 @@ export const ProModal = () => {
               <li>And more!</li>
             </ul>
           </div>
-          <Button className="w-full" variant="primary">
+          <Button
+            onClick={onClick}
+            disabled={isLoading}
+            className="w-full"
+            variant="primary"
+          >
             Upgrade
           </Button>
         </div>
